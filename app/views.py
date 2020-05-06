1f082542142510be717e7d00412df489767fcb45
mimagiera <michalmagierra@gmail.com>
2020-05-06 21:30:13 +0200

    update title

--- app/views.py
+++ app/views.py
@@ -40,8 +40,7 @@ def fetch_posts():
 def index():
     fetch_posts()
     return render_template('index.html',
-                           title='Smart manufacturing PoC'
-                                 'content sharing',
+                           title='Smart manufacturing PoC',
                            posts=posts,
                            node_address=CONNECTED_NODE_ADDRESS,
                            readable_time=timestamp_to_string)
